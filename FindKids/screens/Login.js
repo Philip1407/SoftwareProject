4ccf55d93bc44c77ab2e1a953b08641a8ffdce98
ndbSon <49903834+ndbSon@users.noreply.github.com>
2020-04-13 16:49:00 +0700

    create findKids
    create findKids

---
+++ FindKids/screens/Login.js
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+import {Text} from 'react-native';
+import {ScrollView } from 'react-native-gesture-handler';
+
+export default function LinksScreen() {
+  return (
+    <ScrollView >
+      <Text>anh son oi</Text>
+    </ScrollView>
+  );
+}
+
+
