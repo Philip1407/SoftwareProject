8819f5280f915c1c71647ed07113bd8f054c5201
ndbSon <49903834+ndbSon@users.noreply.github.com>
2020-05-01 22:34:27 +0700

    fix connect mysql

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,25 +1,9 @@
 let db = require('../utils/db')
 
-let User={}
-
-User.find = async(condition) =>{
-    return await db.find('user', condition)
-}
-
-User.findAll = async()=>{
-        return db.findAll('user')
-    }
-User.create= async(user)=>{
-        const query = await db.create('user','username, password, firstname, lastname, role', user)
-        query.msg = "Sign in successful"
-        return query
-    }
-User.update = async(user, condition)=>{
-    return db.update('user',user,condition)
-}
-User.delete = async(condition)=>{
-    return db.delete('user', condition)
-}
-
-module.exports= User
-
+module.exports = {
+    find:  condition => db.find('user', condition),
+    findAll: _=> db.findAll('user'),
+    create: entity=>db.create(entity,'user'),
+    update: (entity,id)=>{db.update(entity,'user','id',id)},
+    delete: id=>{'user','id',id}
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -23,7 +23,7 @@ router.post('/signup', async function (req,res,next){
   res.status(result.status).send(result.msg)
 })
 
-router.get('/signin', async function (req,res,next){
+router.get('/signup', async function (req,res,next){
   let result = await User.find(`username='${req.body.username}' && password='${req.body.password}'`)
   if(result.status===400) res.status(result.status).send(result.err)
   res.status(result.status).send(result.result)
