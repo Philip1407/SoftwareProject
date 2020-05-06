b2446782113c3dad5f03453e124c6408f8a0415f
ndbSon <49903834+ndbSon@users.noreply.github.com>
2020-05-07 00:09:01 +0700

    download record to server to parent client
    download record to server to parent client

--- FindKids/action/index.js
+++ FindKids/action/index.js
@@ -1,12 +1,14 @@
 // import * as Axios from '../axios/ApiAxios'
 import axiosService from '../axios/axios'
 import { apiAxios } from "../constants";
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
 export const login = (user) => {// đăng nhập
     let login = apiAxios.LOGIN;
     return async () => {
         // let data = await axiosService.post(login,user);
         // console.log("apiAxios",data) //back end trả giá trị data về
-        return {status :200}
+        return { status: 200 }
     }
 }
 
@@ -17,22 +19,34 @@ export const signup = (user) => { // đăng ký
         // let data = await axiosService.post(signup,user);
         // console.log("apiAxios",data) //back end trả giá trị data về
         console.log(user)
-        return {status :200}
+        return { status: 200 }
     }
 }
 
-export const uploadRecord = (data) => { 
+export const uploadRecord = (data) => {
     let uploadRecord = apiAxios.UPLOADRECORD;
     console.log(uploadRecord)
     return async () => {
-    //    let abc= await axiosService.post(uploadRecord,{data});
-    //    console.log("apiAxios",abc.data) //back end trả giá trị data về
+        //    let abc= await axiosService.post(uploadRecord,{data});
+        //    console.log("apiAxios",abc.data) //back end trả giá trị data về
         //  console.log("abc",data)
-        let abc= await fetch(uploadRecord, {
+        await fetch(uploadRecord, {
             method: 'POST',
             body: data
-            });
-            console.log('Cloudinary Info:',await abc.json() )
-          }
-      
+        });
+        // console.log('Cloudinary Info:',await abc.json());
+        let uri = await FileSystem.downloadAsync(
+            uploadRecord + "/down",
+            FileSystem.documentDirectory + 'small.m4a'
+        )
+        // console.log("day la uri: ",uri)   
+        let newAsset = await MediaLibrary.createAssetAsync(uri.uri);
+        //create an album on the device in to which the recordings should be stored, and pass in the new asset to store
+        MediaLibrary.createAlbumAsync('Recording', newAsset)
+          .then(() => { console.log('Album created!')
+                        })
+          .catch(err => console.log('Album creation error', err));
+    }
+    
+    
 }
--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -3,18 +3,39 @@ const formidable = require('formidable');
 const fs =require("fs")
 var router = express.Router();
 
-
+var tenpath='';
 router.post('/', function(req, res, next) {
    const form = formidable({ multiples: true, uploadDir:"./public/Records",keepExtensions:true });
-   form.parse(req, (err, fields, files) => {
+  //  let tenpath='';
+    form.parse(req, (err, fields, files) => {
      if(err){
       console.log("false");
      }
+    // tenpath=files.file.path;
+    console.log(files.file.path)
+    tenpath=files.file.path;
     // console.log('fields:', fields);
-   // console.log('files:', files);
-    fs.rename(files.file.path,files.file.path+'.m4a')
+    // console.log('files:', files);
+    fs.rename(files.file.path,files.file.path+'.m4a', function (err) {
+      if (err) {
+          console.log(err);
+      }})
   });
+  // console.log("path: ",tenpath);
+  // res.sendFile(tenpath, function (err) {
+  //   if (err) {
+  //     console.log(err)
+  //     next(err)
+  //   } else {
+  //     console.log('Sent:', fileName)
+  //   }
+  // })
    res.status(201).json("server co tra ve gia tri");
+  
 });
+router.get('/down', function(req, res, next) {
+   res.download(tenpath+".m4a");
+  // res.download("public/Records/upload_4791f796eaa1138bbba928466cf0466b.mp3");
+})
 
 module.exports = router;
