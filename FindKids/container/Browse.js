5189cf08c1cfef964894842f501a125b815665b5
huuthangnguyen98 <huuthangnguyen98@gmail.com>
2020-05-26 22:30:38 +0700

    create code Phong

--- FindKids/container/Browse.js
+++ FindKids/container/Browse.js
@@ -1,37 +1,42 @@
-import React, { Component } from 'react';
-import BrowseScreens from '../screens/Browse'
-import { Block} from "../components";
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux';
-import {login as LoginAPI} from '../action'
-
+import React, { Component } from "react";
+import BrowseScreens from "../screens/Browse";
+import { Block } from "../components";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { login as LoginAPI } from "../action";
 
 class Browse extends Component {
     render() {
-        const {navigation}=this.props;
+        const { navigation } = this.props;
         return (
             <Block>
-                <BrowseScreens navigation={navigation} QuanLyTre={this.QuanLyTre}></BrowseScreens>
+                <BrowseScreens
+                    navigation={navigation}
+                    QuanLyTre={this.QuanLyTre}
+                ></BrowseScreens>
             </Block>
         );
     }
 
-    QuanLyTre=()=>{
-        var {socket}=this.props;
-        console.log("QuanLyTre + ten máy con")  //lấy id máy con vào db
-        let data ={Phong: "UserPH",id:socket.id}
-        socket.emit("QuanLyTre",JSON.stringify(data))  //UserPH lấy từ db
-      }
+    QuanLyTre = () => {
+        var { socket } = this.props;
+        console.log("QuanLyTre + ten máy con"); //lấy id máy con vào db
+        let Phong = Math.floor(Math.random() * 100000) + 1;
+        let data = { Phong, id: socket.id };
+        console.log(data);
+
+        socket.emit("QuanLyTre", JSON.stringify(data)); //UserPH lấy từ db
+    };
 }
 const mapStatetoProps = (state) => {
     return {
         socket: state.socket,
-    }
-}
+    };
+};
 const mapDispatchtoProps = (dispatch) => {
     return {
-        LoginUser : bindActionCreators(LoginAPI, dispatch),
-    }
-}
+        LoginUser: bindActionCreators(LoginAPI, dispatch),
+    };
+};
 
-export default connect(mapStatetoProps,mapDispatchtoProps)(Browse);
+export default connect(mapStatetoProps, mapDispatchtoProps)(Browse);
